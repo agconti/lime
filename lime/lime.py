1dca5afc14f7a58853aa0c88ec349349540dcf57
agconti <agc11d@gmail.com>
2014-10-10 19:16:14 -0400

    refactor(lime/get_trade_history): sorted and cleaned methods

--- lime/lime.py
+++ lime/lime.py
@@ -26,6 +26,20 @@ class Lime:
         ''' Returns the exchange chosen '''
         return self._exchange
 
+    def get_df(self):
+        ''' Gets the stored tick data '''
+        return self._df
+
+    def set_df(self, dataframe):
+        '''
+        Sets stored tick data
+        
+        Parameters
+        * dataframe -- pandas.DataFrame()
+        '''
+        self._df = dataframe
+        self.process_data()
+
     def initialize_start_date(self, date):
         '''
         Returns parsed todays date, a parsed supplied date
@@ -46,7 +60,7 @@ class Lime:
             raise LimeInvaildDate(start, end)
         return True
 
-    def check_ticker_exchange_extenstion(self):
+    def validate_ticker_exchange_extenstion(self):
         ''' Checks if ticker has a valid exchange extension. '''
         extension = self.ticker.split('.')[1]
         if extension == ('O' or 'N' or 'A'):
@@ -58,7 +72,7 @@ class Lime:
         modifies ticker with correct exchange extension
         '''
         try:
-            self.check_ticker_exchange_extenstion()
+            self.validate_ticker_exchange_extenstion()
         except IndexError:
             ## TODO: Needs to be refactored
             self.ticker = "{}{}".format(self.ticker,
@@ -73,12 +87,37 @@ class Lime:
         '''
         for key in self._exchanges.keys():
             self.ticker = "{}{}".format(self.ticker, self._exchanges[key])
-            self._df = self.get_tick_data()
+            self.get_tick_data()
             if self._df is not None and (len(self._df.columns) > 1):
                 return self._exchange = key
 
         raise LimeInvaildTicker()
 
+    def set_start_end_dates(self, start, end=None):
+        '''
+        Parses and Prepares Start and End dates.
+
+        ###Parameters
+        * start -- datetime
+        * end -- ( optional ) datetime, defaults to today's date
+        '''
+        self.check_date(start, end)
+        self.start_date = self.date_parse(start)
+        self.end_date = self.date_parse(end) if end else self.get_date_today()
+    
+    def process_data(self):
+        '''
+        Cleans data after its retrived from Netfonds
+        '''
+        df = self.get_df()
+        try:
+            df.time = df.time.apply(lambda x: datetime.datetime.strptime(x, '%Y%m%dT%H%M%S'))
+            df = df.set_index(df.time)
+        except AttributeError:
+            raise LimeInvaildQuery()
+
+        self.set_df(df)
+   
     def get_tick_data(self):
         '''
         Retrives tick data from Netfonds from a known ticker.
@@ -87,17 +126,17 @@ class Lime:
                                                          self.start_date,
                                                          self.ticker,
                                                          self._file_format)
-        return read_csv(uri)  
+        self.set_df(read_csv(uri))
 
-    
     def get_trades(self, ticker):
         '''
         Gets the trades made for a ticker on a specified day.
         '''
         self.ticker = ticker
+        self.check_ticker_exchange_extenstion()
         
         if self._exchange_found:
-            self._df = self.get_tick_data()
+            self.get_tick_data()
         
         # finds the correct ticker if its not found
         if not self._exchange_found:
@@ -105,20 +144,15 @@ class Lime:
                 self._df = self.get_exchange_from_ticker()
             else:
                 self.ticker = self.exchange_tracker()
-                self._df = self.get_tick_data()
+                self.get_tick_data()
         
-        # cleans data after its retrived
-        if self._df is not None:
-            self._df.time = self._df.time.apply(lambda x: datetime.datetime.strptime(x, '%Y%m%dT%H%M%S'))
-            self._df = self._df.set_index(self._df.time)
-        else:
-            raise LimeInvaildQuery()
-
-    def get_trade_history(self, begining_date, ticker, end_date):
+        return self.get_df()
+    
+    def get_trade_history(self, ticker, start_date, end_date=None):
         '''
         Gets the trades made for a ticker from a range of days.
 
-        self, begining_date, end_date (optional), ticker
+        ticker, start_date, end_date (optional)
 
         start date is the day you want to start exctracting info from
 
@@ -131,23 +165,19 @@ class Lime:
 
         If no end_date is specified then it defaults today
         '''
+        
+        #TODO: revamp setting and Getting
+
         self.ticker = ticker
-        self.end_date = end_date
         self._exchange_found = False
-        self.check_date(start_date, end_date)
+        self.set_start_end_dates(start_date, end_date)
 
-        # prep dates
-        begining_date = self.date_parse(begining_date)
-
-        if end_date == '':
-            self.end_date = self.get_date_today()
-        else:
-            self.end_date = self.date_parse(end_date)
-
-        # Get stock tick data
-        for day in date_range(start=begining_date, end=self.end_date, freq='B'):
+        for day in date_range(start=start_date, end=self.end_date, freq='B'):
             self.start_date = self.date_parse(day)
-            if self._df is not None:
-                concat([self._df, self.get_trades(self.ticker)])
+
+            if self.get_df():
+                self.set_df(concat([self.get_df(), self.get_trades(self.ticker)]))
             else:
-                self._df = self.get_trades(self.ticker)
+                self.set_df(self.get_trades(self.ticker))
+
+        return self.get_df()
