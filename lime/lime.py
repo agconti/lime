c56ff700821da44a369dae0c02a1d61a2e41958e
agconti <agc11d@gmail.com>
2014-10-11 20:06:55 -0400

    refactor(lime/get_trades): updated df setter and ticker validation

--- lime/lime.py
+++ lime/lime.py
@@ -20,6 +20,7 @@ class Lime:
             'Nyse': '.N',
             'Amex': '.A'
         }
+        self.exchange_extensions = ['O', 'N', 'A']
         self._url = 'http://www.netfonds.no/quotes/tradedump.php'
         
     def get_exchange(self):
@@ -37,12 +38,15 @@ class Lime:
         Parameters
         * dataframe -- pandas.DataFrame()
         '''
-        self._df = dataframe
+        self._df = concat([self.get_df(), dataframe]) if self._df else dataframe
         self.process_data()
 
     def initialize_start_date(self, date):
         '''
         Returns parsed todays date, a parsed supplied date
+
+        ###Parameters
+        * date -- datetime, date to be parsed
         '''
         if not date:
             date = datetime.date.today()
@@ -50,46 +54,66 @@ class Lime:
 
     def date_parse(self, date):
         '''
-        Parses data to YYYY/MM/DD
+        Parses date to YYYY/MM/DD.
+
+        ###Parameters
+        * date -- datetime, date to be parsed
         '''
         return date.strftime('%Y%m%d')
 
     def check_date(self, start, end):
-        ''' Checks wether supplied dates are accpable. '''
+        '''
+        Checks whether supplied dates are acceptable.
+
+        ###Parameters
+        * start -- datetime, date beginning the retrieval window
+        * end -- datetime, date ending the retrieval window
+        '''
         if timedelta(0) > (end - start) > timedelta(21):
             raise LimeInvaildDate(start, end)
         return True
 
+    def format_ticker_with_exchange_extenstion(self):
+        self.ticker = "{}{}".format(self.ticker,
+                                    self._exchanges[self._exchange.title()])
+        return self.ticker
+
     def validate_ticker_exchange_extenstion(self):
-        ''' Checks if ticker has a valid exchange extension. '''
+        '''Checks if ticker has a valid exchange extension. '''
         extension = self.ticker.split('.')[1]
-        if extension == ('O' or 'N' or 'A'):
+        if extension in self.exchange_extensions:
             return True
         return False
 
     def check_ticker_exchange_extenstion(self):
         '''
-        modifies ticker with correct exchange extension
+        Check's whether the appropriate netfonds extension, ( '.N', '.O', '.A' ), has been added.
+        If it hasn't, but the ticker's exchange has, it adds the appropriate extension.
+        If neither have; it raises a LimeInvaildTicker exception.
         '''
         try:
             self.validate_ticker_exchange_extenstion()
         except IndexError:
-            ## TODO: Needs to be refactored
-            self.ticker = "{}{}".format(self.ticker,
-                                        self._exchanges[self._exchange.title()])
-            self._exchange_found = True
-            return self.ticker
+            if not self._exchange:
+                self.get_exchange_extension_from_ticker()
+            self.format_ticker_with_exchange_extenstion()
+        else:
+            raise LimeInvaildTicker()
+        
+        return self.ticker
 
-    def get_exchange_from_ticker(self):
+    def get_exchange_extension_from_ticker(self):
         '''
-        Loops through the three exchanges Netfonds supports, Nasdaq; NYSE; Amex,
+        Loops through the three exchanges Netfonds supports, ( Nasdaq, NYSE, Amex),
         and returns the correct exchange extension if it exists.
         '''
         for key in self._exchanges.keys():
             self.ticker = "{}{}".format(self.ticker, self._exchanges[key])
             self.get_tick_data()
             if self._df is not None and (len(self._df.columns) > 1):
-                return self._exchange = key
+                self._exchange = key
+                self.format_ticker_with_exchange_extenstion()
+                return self._exchange
 
         raise LimeInvaildTicker()
 
@@ -107,7 +131,7 @@ class Lime:
     
     def process_data(self):
         '''
-        Cleans data after its retrived from Netfonds
+        Cleans data after its retrieved from Netfonds
         '''
         df = self.get_df()
         try:
@@ -120,7 +144,7 @@ class Lime:
    
     def get_tick_data(self):
         '''
-        Retrives tick data from Netfonds from a known ticker.
+        Retrieves tick data from Netfonds from a known ticker.
         '''
         uri = '{}?date={}&paper={}&csv_format={}'.format(self._url,
                                                          self.start_date,
@@ -131,53 +155,34 @@ class Lime:
     def get_trades(self, ticker):
         '''
         Gets the trades made for a ticker on a specified day.
+
+        ###Parameters
+        * ticker -- string, stock ticker symbol
         '''
         self.ticker = ticker
         self.check_ticker_exchange_extenstion()
-        
-        if self._exchange_found:
-            self.get_tick_data()
-        
-        # finds the correct ticker if its not found
-        if not self._exchange_found:
-            if self._exchange == '':
-                self._df = self.get_exchange_from_ticker()
-            else:
-                self.ticker = self.exchange_tracker()
-                self.get_tick_data()
+        self.get_tick_data()
         
         return self.get_df()
     
     def get_trade_history(self, ticker, start_date, end_date=None):
         '''
-        Gets the trades made for a ticker from a range of days.
+        Retrieves the trades made for a ticker from a range of days.
 
-        ticker, start_date, end_date (optional)
+        ###Parameters
+        * ticker -- string, stock ticker symbol
+        * start_date -- datetime, starting date of retrieval window
+        * end_date  -- datetime (optional), ending date of retrieval window.
+            defaults to today, if committed.
 
-        start date is the day you want to start exctracting info from
-
-        ie extract info :
-
-        (start_date)    (end_date)
-        May 21, 2013 to June 1, 2013
-
-        Tick data only persits for 21 days! Any queries longer than that will fail
-
-        If no end_date is specified then it defaults today
+        Note: Tick data only persist for 21 days on Netfonds. Any queries greater
+        than that window will raise a LimeInvaildQuery exception.
         '''
-        
-        #TODO: revamp setting and Getting
-
         self.ticker = ticker
-        self._exchange_found = False
         self.set_start_end_dates(start_date, end_date)
 
         for day in date_range(start=start_date, end=self.end_date, freq='B'):
             self.start_date = self.date_parse(day)
-
-            if self.get_df():
-                self.set_df(concat([self.get_df(), self.get_trades(self.ticker)]))
-            else:
-                self.set_df(self.get_trades(self.ticker))
+            self.set_df(self.get_trades(self.ticker))
 
         return self.get_df()
